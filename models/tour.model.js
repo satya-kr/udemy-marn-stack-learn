6462b2d6d16a1cf62d60bf5cab47d2fe3d62c86d
satya-kr <satyajit.kr.prajapati@gmail.com>
2025-03-01 20:03:19 +0530

    finish tour model

--- models/tour.model.js
+++ models/tour.model.js
@@ -5,16 +5,54 @@ const tourSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'Tour must have a name'],
-    unique: true
+    unique: true,
+    trim: true
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'Tour must have duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'Tour must have a group size']
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'tour must have a difficulty']
+  },
+  ratingsAverage: {
     type: Number,
     default: 4.5
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0
   },
   price: {
     type: Number,
     required: [true, 'Tour must have price']
-  }
+  },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'tour must have summary']
+  },
+  description: {
+    type: String,
+    trim: true
+  },
+  imageCover: {
+    type: String,
+    trim: true,
+    required: [true, 'tour must have cover image']
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  },
+  startDates: [Date]
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
